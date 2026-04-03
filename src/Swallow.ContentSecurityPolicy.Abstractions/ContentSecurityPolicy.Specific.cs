e47b0983a3432680faa74fcd1f04030bda8a4524
Philipp Kiener <kiener.phil@gmail.com>
2026-04-03 14:44:31 +0200

    So many!

---
+++ src/Swallow.ContentSecurityPolicy.Abstractions/ContentSecurityPolicy.Specific.cs
@@ -0,0 +1,90 @@
+using Swallow.ContentSecurityPolicy.Abstractions.Directives;
+
+namespace Swallow.ContentSecurityPolicy.Abstractions;
+
+public sealed partial class ContentSecurityPolicy
+{
+    public ChildSourceDirective? ChildSource
+    {
+        get => GetSpecific<ChildSourceDirective>(ChildSourceDirective.Name);
+        set => SetOrRemove(ChildSourceDirective.Name, value);
+    }
+
+    public ConnectSourceDirective? ConnectSource
+    {
+        get => GetSpecific<ConnectSourceDirective>(ConnectSourceDirective.Name);
+        set => SetOrRemove(ConnectSourceDirective.Name, value);
+    }
+
+    public DefaultSourceDirective? DefaultSource
+    {
+        get => GetSpecific<DefaultSourceDirective>(DefaultSourceDirective.Name);
+        set => SetOrRemove(DefaultSourceDirective.Name, value);
+    }
+
+    public FontSourceDirective? FontSource
+    {
+        get => GetSpecific<FontSourceDirective>(FontSourceDirective.Name);
+        set => SetOrRemove(FontSourceDirective.Name, value);
+    }
+
+    public FormActionDirective? FormAction
+    {
+        get => GetSpecific<FormActionDirective>(FormActionDirective.Name);
+        set => SetOrRemove(FormActionDirective.Name, value);
+    }
+
+    public FrameAncestorsDirective? FrameAncestors
+    {
+        get => GetSpecific<FrameAncestorsDirective>(FrameAncestorsDirective.Name);
+        set => SetOrRemove(FrameAncestorsDirective.Name, value);
+    }
+
+    public FrameSourceDirective? FrameSource
+    {
+        get => GetSpecific<FrameSourceDirective>(FrameSourceDirective.Name);
+        set => SetOrRemove(FrameSourceDirective.Name, value);
+    }
+
+    public ImageSourceDirective? ImageSource
+    {
+        get => GetSpecific<ImageSourceDirective>(ImageSourceDirective.Name);
+        set => SetOrRemove(ImageSourceDirective.Name, value);
+    }
+
+    public ManifestSourceDirective? ManifestSource
+    {
+        get => GetSpecific<ManifestSourceDirective>(ManifestSourceDirective.Name);
+        set => SetOrRemove(ManifestSourceDirective.Name, value);
+    }
+
+    public MediaSourceDirective? MediaSource
+    {
+        get => GetSpecific<MediaSourceDirective>(MediaSourceDirective.Name);
+        set => SetOrRemove(MediaSourceDirective.Name, value);
+    }
+
+    public ObjectSourceDirective? ObjectSource
+    {
+        get => GetSpecific<ObjectSourceDirective>(ObjectSourceDirective.Name);
+        set => SetOrRemove(ObjectSourceDirective.Name, value);
+    }
+
+    public ScriptSourceDirective? ScriptSource
+    {
+        get => GetSpecific<ScriptSourceDirective>(ScriptSourceDirective.Name);
+        set => SetOrRemove(ScriptSourceDirective.Name, value);
+    }
+
+    public StyleSourceDirective? StyleSource
+    {
+        get => GetSpecific<StyleSourceDirective>(StyleSourceDirective.Name);
+        set => SetOrRemove(StyleSourceDirective.Name, value);
+    }
+
+    public WorkerSourceDirective? WorkerSource
+    {
+        get => GetSpecific<WorkerSourceDirective>(WorkerSourceDirective.Name);
+        set => SetOrRemove(WorkerSourceDirective.Name, value);
+    }
+}
--- src/Swallow.ContentSecurityPolicy.Abstractions/ContentSecurityPolicy.cs
+++ src/Swallow.ContentSecurityPolicy.Abstractions/ContentSecurityPolicy.cs
@@ -1,36 +1,10 @@
-using Swallow.ContentSecurityPolicy.Abstractions.Directives;
-
 namespace Swallow.ContentSecurityPolicy.Abstractions;
 
-public sealed class ContentSecurityPolicy
+public sealed partial class ContentSecurityPolicy
 {
     private readonly Dictionary<string, Directive> directives = [];
 
     public IEnumerable<Directive> Directives => directives.Values.AsEnumerable();
-
-    public DefaultSourceDirective? DefaultSource
-    {
-        get => GetSpecific<DefaultSourceDirective>(DefaultSourceDirective.Name);
-        set => SetOrRemove(DefaultSourceDirective.Name, value);
-    }
-
-    public ChildSourceDirective? ChildSource
-    {
-        get => GetSpecific<ChildSourceDirective>(ChildSourceDirective.Name);
-        set => SetOrRemove(ChildSourceDirective.Name, value);
-    }
-
-    public ConnectSourceDirective? ConnectSource
-    {
-        get => GetSpecific<ConnectSourceDirective>(ConnectSourceDirective.Name);
-        set => SetOrRemove(ConnectSourceDirective.Name, value);
-    }
-
-    public FontSourceDirective? FontSource
-    {
-        get => GetSpecific<FontSourceDirective>(FontSourceDirective.Name);
-        set => SetOrRemove(FontSourceDirective.Name, value);
-    }
 
     public void Add(Directive directive)
     {
